6bece39964e5ef82ed75afa0243137a9df1465b8
Kevin Sunahara <email@fiap.com.br>
2023-12-01 20:33:59 -0300

    First commit

---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.83.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features{}
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "rg_sb_eastus_23714_1_170146507185"
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = "rg_sb_eastus_23714_1_170146507185"
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = "East US"
+  resource_group_name = "rg_sb_eastus_23714_1_170146507185"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "vm-kevin"
+  resource_group_name = "rg_sb_eastus_23714_1_170146507185"
+  location            = "East US"
+  size                = "standard_b1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
